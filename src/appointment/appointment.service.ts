288b7393e206be0161d2d807f8c3e94532ee9952
fadhilmauladhani <fadhilmauladhani@gmail.com>
2024-07-16 13:44:01 +0700

    add 2 test

---
+++ src/appointment/appointment.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Appointment } from './appointment.model';
+
+export interface AppointmentInput {
+  patientId: number;
+  startTime: Date;
+  endTime: Date;
+}
+
+@Injectable()
+export class AppointmentService {
+  public scheduleAppointment(appointmentData: AppointmentInput): Appointment {
+
+    if (appointmentData.endTime < appointmentData.startTime) {
+        throw new Error("appointment's endTime should be after startTime");
+    }
+
+    return {
+      ...appointmentData,
+      confirmed: false,
+    };
+  }
+}
